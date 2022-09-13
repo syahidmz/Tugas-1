b8f52709b42cefb084aeafba714d50968e82b9f2
syahidmz <syahiduu0@gmail.com>
2022-09-13 20:01:29 +0700

    first commit

---
+++ Tugas 6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        for (int i = 1; i <= 5; ++i) {
+        cout << i << " Belajar Koding Di Kelas Algoritma " << endl;
+    }
+    return 0;
+}
