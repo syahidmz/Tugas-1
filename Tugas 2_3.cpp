93afac0b213aaa1b05e1c8d1c33bc5608d14ad79
syahidmz <113004243+syahidmz@users.noreply.github.com>
2022-09-20 23:15:09 +0700

    Add files via upload

---
+++ Tugas 2_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define harga 4500.02
+
+int main ()
+
+{
+  float jumlah, total;
+  cout << "Masukan jumlah barang = ";
+  cin >> jumlah;
+  total = harga*jumlah;
+    cout << "Yang harus di bayar = " << total << endl;
+
+
+}
